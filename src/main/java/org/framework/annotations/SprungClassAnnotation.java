17bbd3529ebf1909f9dc5bda67642654cb6a4861
Alfred <alfredboahene.7@gmail.com>
2024-08-08 15:02:43 -0500

    add sprungboot app annotation

--- src/main/java/org/framework/annotations/SprungClassAnnotation.java
+++ src/main/java/org/framework/annotations/SprungClassAnnotation.java
@@ -3,7 +3,8 @@ package org.framework.annotations;
 import java.lang.annotation.Annotation;
 
 public enum SprungClassAnnotation {
-    SERVICE(Service.class);
+    SERVICE(Service.class),
+    SPRING_BOOT_APPLICATION(SprungBootApplication.class);
 
     private final Class<? extends Annotation> annotationClass;
 
